1fd4200605cc02455d55b3ab0be8dc92bd74f273
Michael V <michaelvrld@gmail.com>
2023-04-08 14:58:18 +0200

    Convert indentation to spaces

--- live.php
+++ live.php
@@ -1,85 +1,85 @@
 <?php
-	define("PATH_JSON", "/home/samp/lmdmtest/scriptfiles/positions.json");
-	define("API_KEY", "");
+    define("PATH_JSON", "/home/samp/lmdmtest/scriptfiles/positions.json");
+    define("API_KEY", "");
 
-	$json_pos = file_get_contents(PATH_JSON);
+    $json_pos = file_get_contents(PATH_JSON);
 
-	if(empty(API_KEY))
-	    die("You need a google api key first.");
+    if(empty(API_KEY))
+        die("You need a google api key first.");
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="refresh" content="60" />
-		<title>SA:MP Server Graph</title>
-    	<style type="text/css">
-        	#map-canvas { display: inline-block; height: 700px; width: 700px; }
-        	#map-legend { padding: 10px; background-color: rgba(141, 142, 127, 0.46);}
-    	</style>
-	</head>
+    <head>
+        <meta http-equiv="refresh" content="60" />
+        <title>SA:MP Server Graph</title>
+        <style type="text/css">
+            #map-canvas { display: inline-block; height: 700px; width: 700px; }
+            #map-legend { padding: 10px; background-color: rgba(141, 142, 127, 0.46);}
+        </style>
+    </head>
 
-	<body>
+    <body>
         <div id="map-canvas"></div>
         <div id="map-legend">
-        	<h2>Legend</h2>
-        	<p><img src="http://wiki.sa-mp.com/wroot/images2/1/15/Icon_56.gif" /> Players (click to view data)</p>
+            <h2>Legend</h2>
+            <p><img src="http://wiki.sa-mp.com/wroot/images2/1/15/Icon_56.gif" /> Players (click to view data)</p>
         </div>
 
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-		<script src="http://maps.google.com/maps/api/js?sensor=false&key=<?php echo API_KEY; ?>"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+        <script src="http://maps.google.com/maps/api/js?sensor=false&key=<?php echo API_KEY; ?>"></script>
         <script src="js/SanMap.min.js"></script>
-		<script type="text/javascript">
-			var p_pos = <?php echo (empty($json_pos)) ? "" : $json_pos ?>;
+        <script type="text/javascript">
+            var p_pos = <?php echo (empty($json_pos)) ? "" : $json_pos ?>;
 
-			//for each full gta sa map in whatever resolution/color/type/.. you have you should make a type for it
+            //for each full gta sa map in whatever resolution/color/type/.. you have you should make a type for it
 
-			var mapType = new SanMapType(0, 1, function (zoom, x, y) {
-		        return x == -1 && y == -1 
-				? "images/tiles/map.outer.png" 
-				: "images/tiles/map." + zoom + "." + x + "." + y + ".png";
-		    });
-		    
-		    var satType = new SanMapType(0, 3, function (zoom, x, y) {
-		        return x == -1 && y == -1 
-				? null 
-				: "images/tiles/sat." + zoom + "." + x + "." + y + ".png";
-		    });
-		    
-		    var map = SanMap.createMap(
-		        document.getElementById('map-canvas'),
-				{'Map': mapType, 'Satellite': satType}, //specify all your types from above here
+            var mapType = new SanMapType(0, 1, function (zoom, x, y) {
+                return x == -1 && y == -1 
+                ? "images/tiles/map.outer.png" 
+                : "images/tiles/map." + zoom + "." + x + "." + y + ".png";
+            });
+            
+            var satType = new SanMapType(0, 3, function (zoom, x, y) {
+                return x == -1 && y == -1 
+                ? null 
+                : "images/tiles/sat." + zoom + "." + x + "." + y + ".png";
+            });
+            
+            var map = SanMap.createMap(
+                document.getElementById('map-canvas'),
+                {'Map': mapType, 'Satellite': satType}, //specify all your types from above here
                 2,
                 SanMap.getLatLngFromPos(0,0), false, 'Satellite'
             );
 
-		    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(document.getElementById('map-legend'));
+            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(document.getElementById('map-legend'));
 
-		    if(p_pos !== "")
-		    	for (var i = 0; i < Object.keys(p_pos).length; i++) 
-		    		if(p_pos[i].online == 1) createMarker(i); 
+            if(p_pos !== "")
+                for (var i = 0; i < Object.keys(p_pos).length; i++) 
+                    if(p_pos[i].online == 1) createMarker(i); 
 
-			google.maps.event.addListener(map, 'click', function(event) {
-				var pos = SanMap.getPosFromLatLng(event.latLng);
-		    }); 
+            google.maps.event.addListener(map, 'click', function(event) {
+                var pos = SanMap.getPosFromLatLng(event.latLng);
+            }); 
 
-			function createMarker(id)
-			{
-				var p_windows = new google.maps.InfoWindow({
-		    		content: "<p>"+p_pos[id].name+" <b>(ID: "+id+")</b><br>Ping: "+p_pos[id].ping+"</p>"
-		    	});
+            function createMarker(id)
+            {
+                var p_windows = new google.maps.InfoWindow({
+                    content: "<p>"+p_pos[id].name+" <b>(ID: "+id+")</b><br>Ping: "+p_pos[id].ping+"</p>"
+                });
 
-		    	var p_marker = new google.maps.Marker({
-		    		position: SanMap.getLatLngFromPos(p_pos[id].x, p_pos[id].y),
-		    		map: map,
-		    		icon: "http://wiki.sa-mp.com/wroot/images2/1/15/Icon_56.gif"
-		    	});
+                var p_marker = new google.maps.Marker({
+                    position: SanMap.getLatLngFromPos(p_pos[id].x, p_pos[id].y),
+                    map: map,
+                    icon: "http://wiki.sa-mp.com/wroot/images2/1/15/Icon_56.gif"
+                });
 
-				google.maps.event.addListener(p_marker, 'click', function() {
-					p_windows.open(map,p_marker);
-				});
-			}
-		</script>
-	</body>
+                google.maps.event.addListener(p_marker, 'click', function() {
+                    p_windows.open(map,p_marker);
+                });
+            }
+        </script>
+    </body>
 </html>
--- live.pwn
+++ live.pwn
@@ -14,63 +14,63 @@ new gTimer,Name[MAX_PLAYERS][MAX_PLAYER_NAME];
 
 public OnFilterScriptInit()
 {
-	print("\n--------------------------------------");
-	print("SA:MP Live Map");
-	print("--------------------------------------\n");
+    print("\n--------------------------------------");
+    print("SA:MP Live Map");
+    print("--------------------------------------\n");
 
-	gTimer = SetTimer("OnUpdateJSON", UPDATE_TIME, true);
+    gTimer = SetTimer("OnUpdateJSON", UPDATE_TIME, true);
 
-	djson_GameModeInit();
-	return 1;
+    djson_GameModeInit();
+    return 1;
 }
 
 public OnFilterScriptExit()
 {
-	djson_GameModeExit();
-	KillTimer(gTimer);
-	return 1;
+    djson_GameModeExit();
+    KillTimer(gTimer);
+    return 1;
 }
 
 public OnPlayerConnect(playerid)
 {
-	GetPlayerName(playerid, Name[playerid], MAX_PLAYER_NAME);
-	return 1;
+    GetPlayerName(playerid, Name[playerid], MAX_PLAYER_NAME);
+    return 1;
 }
 
 public OnPlayerDisconnect(playerid, reason)
 {
-	Name[playerid][0] = EOS;
-	return 1;
+    Name[playerid][0] = EOS;
+    return 1;
 }
 
 public OnUpdateJSON()
 {
-	new Float:pos[3],string[16];
-	for(new i = 0; i < MAX_PLAYERS; i++)
-	{
-		if(!IsPlayerConnected(i))
-		{
-			format(string,sizeof(string),"%d/online",i);
-			djSetInt(FILE_JSON,string,0);
-		}
-		else
-		{
-			GetPlayerPos(i, pos[0],pos[1],pos[2]);
+    new Float:pos[3],string[16];
+    for(new i = 0; i < MAX_PLAYERS; i++)
+    {
+        if(!IsPlayerConnected(i))
+        {
+            format(string,sizeof(string),"%d/online",i);
+            djSetInt(FILE_JSON,string,0);
+        }
+        else
+        {
+            GetPlayerPos(i, pos[0],pos[1],pos[2]);
 
-			format(string,sizeof(string),"%d/x",i);
-			djSetFloat(FILE_JSON,string,pos[0]);
+            format(string,sizeof(string),"%d/x",i);
+            djSetFloat(FILE_JSON,string,pos[0]);
 
-			format(string,sizeof(string),"%d/y",i);
-			djSetFloat(FILE_JSON,string,pos[1]);
+            format(string,sizeof(string),"%d/y",i);
+            djSetFloat(FILE_JSON,string,pos[1]);
 
-			format(string,sizeof(string),"%d/name",i);
-			djSet(FILE_JSON,string,Name[i]);
+            format(string,sizeof(string),"%d/name",i);
+            djSet(FILE_JSON,string,Name[i]);
 
-			format(string,sizeof(string),"%d/ping",i);
-			djSetInt(FILE_JSON,string,GetPlayerPing(i));
+            format(string,sizeof(string),"%d/ping",i);
+            djSetInt(FILE_JSON,string,GetPlayerPing(i));
 
-			format(string,sizeof(string),"%d/online",i);
-			djSetInt(FILE_JSON,string,1);
-		}
-	}
+            format(string,sizeof(string),"%d/online",i);
+            djSetInt(FILE_JSON,string,1);
+        }
+    }
 }
